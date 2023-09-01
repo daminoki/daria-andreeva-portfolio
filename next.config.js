34f889fd4b3a9fc2b48de6de04259f24bb83fad0
Daria Andreeva <dd1556@mail.ru>
2023-09-01 11:26:01 +0400

    change next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   sassOptions: {
     additionalData: '@import "src/styles/mixins.scss";',
   },
+  images: {
+    domains: ['cheery-sundae-aa15d8.netlify.app'],
+  },
 };
 
 module.exports = nextConfig;
