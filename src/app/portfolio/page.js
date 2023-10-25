835545519c9e5188b216cf33b113ef80a223722d
Daria Andreeva <dd1556@mail.ru>
2023-10-25 17:39:01 +0400

    update portfolio page, add fetch data from sanity

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -1,10 +1,33 @@
 import styles from './page.module.scss';
 import Container from '@/components/ui/Container';
+import { createClient } from 'next-sanity';
 
-export default function Portfolio() {
+export default async function Portfolio() {
+  const projects = await getData();
+
   return <div className={styles.portfolio}>
     <Container>
-    my portfolio
+      <h1 className={styles.portfolio__title}>Portfolio</h1>
+      {projects.length > 0 && (
+        <ul>
+          {projects.map((project) => (
+            <li key={project._id}>{project?.title}</li>
+          ))}
+        </ul>
+      )}
+      {!projects.length > 0 && <p>No projects to show</p>}
     </Container>
   </div>;
+}
+
+const client = createClient({
+  projectId: 'r24w0ip6',
+  dataset: 'production',
+  apiVersion: '2022-03-25',
+  useCdn: false
+});
+export async function getData() {
+  const projects = await client.fetch('*[_type == "project"]');
+
+  return projects;
 }
